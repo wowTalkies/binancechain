1b2ea1c01a0c948e7e63b7955676710100930f9e
Immanuel-1610 <immanuel@wowtalkies.com>
2023-04-28 11:05:04 +0530

    faq screen added

---
+++ front-end/lib/screens/faq/faq_sreen.dart
@@ -0,0 +1,72 @@
+import 'package:bnbapp/screens/faq/cubit/faq_cubit.dart';
+import 'package:bnbapp/screens/faq/cubit/faq_state.dart';
+import 'package:bnbapp/utils/colors.dart';
+import 'package:bnbapp/widgets/appbar.dart';
+import 'package:bnbapp/widgets/faq_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FAQ extends StatelessWidget {
+  const FAQ({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.read<FAQCubit>();
+    return Scaffold(
+      body: BlocListener(
+        bloc: cubit,
+        listener: (context, state) {
+          if (state is FAQErrorState) {
+            if (!state.error.contains('404')) {
+              const SnackBar(
+                content: Text("error"),
+              );
+            }
+          }
+        },
+        child: const _LayOut(),
+      ),
+    );
+  }
+}
+
+class _LayOut extends StatelessWidget {
+  const _LayOut({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final cubit = context.read<FAQCubit>();
+    return Container(
+      decoration: BoxDecoration(
+          gradient:
+              LinearGradient(colors: [AllColor.linear1, AllColor.linear2])),
+      child: Column(
+        children: [
+          CustomAppBar(
+              title: "Frequently Asked Questions",
+              icon: Icon(
+                Icons.arrow_back_ios_new_rounded,
+                color: AllColor.black,
+              )),
+          Expanded(
+            child: ListView(
+              children: [
+                Column(
+                  children: [
+                    FAQWidget(
+                      containerHeight: 300,
+                      dividerColor: AllColor.faqDividerColor,
+                      itemCount: cubit.faqModel.length,
+                      question: cubit.faqModel,
+                      textColor: AllColor.linear1,
+                    )
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
