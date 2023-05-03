2b55f1b70d7c0ef9e018836dcaacbcf82d63a85a
Immanuel-1610 <immanuel@wowtalkies.com>
2023-05-03 19:02:37 +0530

    updated contract method

--- contracts/contracts/wowTQuiz.sol
+++ contracts/contracts/wowTQuiz.sol
@@ -18,18 +18,19 @@ contract WowTQuiz is OwnableUpgradeable {
     //Points conracts
     WowTPoints private points;
     //Structure to hold Questions and answers. correct index needs to be encripted
-    struct Question {
-        string question;
-        string[4] options;
-        bytes32 answer;
-    }
+    // struct Question {
+    //     string question;
+    //     string[4] options;
+    //     bytes32 answer;
+    // }
 
     //Quiz structure
     struct Quiz {
         string description;
         string imageUrl;
-        address[] quizattenders;
-        Question question;
+        string question;
+        string[4] options;
+        bytes32 answer;
         address creatorAddress;
     }
     // Quiz[] public quizes;
@@ -37,7 +38,7 @@ contract WowTQuiz is OwnableUpgradeable {
     mapping(string => Quiz) public quizmap;
 
     // for checking answer
-    event Answer(bool answer);
+    event Answer(bool indexed);
 
     function initialize(
         uint16 _quizePoints,
@@ -57,9 +58,12 @@ contract WowTQuiz is OwnableUpgradeable {
     //Create Quiz
     function createQuiz(
         string memory _quizName,
-        Question memory _question,
+        // Question memory _question,
         string memory _description,
         string memory _imageUrl,
+        string memory _question,
+        string[4] memory _options,
+        bytes32 _answer,
         address _creatorAddress
     ) external onlyOwner {
         uint256 userPoints = points.getPoints(_creatorAddress);
@@ -69,6 +73,8 @@ contract WowTQuiz is OwnableUpgradeable {
         );
         quizmap[_quizName].description = _description;
         quizmap[_quizName].question = _question;
+        quizmap[_quizName].options = _options;
+        quizmap[_quizName].answer = _answer;
         quizmap[_quizName].imageUrl = _imageUrl;
         quizmap[_quizName].creatorAddress = _creatorAddress;
     }
@@ -89,10 +95,10 @@ contract WowTQuiz is OwnableUpgradeable {
         Quiz memory qtemp = getQuizdetails(_quizName);
         string memory tempDescription = qtemp.description;
         string memory tempImageUrl = qtemp.imageUrl;
-        Question memory tempQuestion = qtemp.question;
+        // Question memory tempQuestion = qtemp.question;
 
-        string memory tQuestion = tempQuestion.question;
-        string[4] memory options = tempQuestion.options;
+        string memory tQuestion = qtemp.question;
+        string[4] memory options = qtemp.options;
 
         return (tempDescription, tempImageUrl, tQuestion, options);
     }
@@ -103,31 +109,54 @@ contract WowTQuiz is OwnableUpgradeable {
         bytes32 choice
     ) public onlyOwner {
         Quiz memory qtemp = getQuizdetails(_quizName);
-        Question memory tempQuestion = qtemp.question;
-        bytes32 answer = tempQuestion.answer;
+        // Question memory tempQuestion = qtemp.question;
+        bytes32 answer = qtemp.answer;
         // if (keccak256(bytes(answer)) == keccak256(bytes(choice))){
+        quizeEvaluatePoints(answer, choice, _userAddress, qtemp.creatorAddress);
+        //return _userAddress; //Need implementation
+    }
+
+    function checkAnswer(
+        string memory _quizName,
+        bytes32 choice
+    ) public view returns (bool) {
+        Quiz memory qtemp = getQuizdetails(_quizName);
+        // Question memory tempQuestion = qtemp.question;
+        bytes32 answer = qtemp.answer;
+        if (answer == choice) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function quizeEvaluatePoints(
+        bytes32 answer,
+        bytes32 choice,
+        address _userAddress,
+        address _creatorAddress
+    ) private {
         if (answer == choice) {
             points.addPoints(_userAddress, pointsToAnswer);
-            points.addPoints(qtemp.creatorAddress, quizePoints);
             emit Answer(true);
         } else {
             emit Answer(false);
         }
-        //return _userAddress; //Need implementation
+        points.addPoints(_creatorAddress, quizePoints);
     }
 
     /*
-   function addQuizePoints( address _userAddress,  uint16 _quizePoints) public onlyOwner{
-       //addPoints(userAddress, quizePoints)
-       points.addPoints(_userAddress, _quizePoints);
+    function addQuizePoints( address _userAddress,  uint16 _quizePoints) public onlyOwner{
+        //addPoints(userAddress, quizePoints)
+        points.addPoints(_userAddress, _quizePoints);
 
-   }
+    }
 
-   function addPointsToAnswer(address _userAddress, uint16 _pointsToAnswer) public onlyOwner{
-       //addPoints(userAddress, pointsToAnswer)
-        points.addPoints(_userAddress, _pointsToAnswer);
+    function addPointsToAnswer(address _userAddress, uint16 _pointsToAnswer) public onlyOwner{
+        //addPoints(userAddress, pointsToAnswer)
+            points.addPoints(_userAddress, _pointsToAnswer);
 
-   }  */
+    }  */
 
     // function encodeAnswer(string memory _input) public pure returns (bytes32) {
     //     bytes32 answerHash = keccak256(abi.encodePacked(_input));
